112cc63e014b0b478a5c12a8095b37e4d144d4ab
santosvitoraugusto <38479273+santosvitoraugusto@users.noreply.github.com>
2026-08-13 01:10:01 -0300

    Versão 18 - inclusão de teste frágil do modal

---
+++ e2e/modal-fragil.spec.js
@@ -0,0 +1,35 @@
+import { test, expect } from '@playwright/test';
+
+test('modal frágil usando estrutura e texto', async ({ page }) => {
+  await page.goto('http://localhost:5173');
+
+  await page.locator('input').nth(2)
+    .fill('admin@test.com');
+
+  await page.locator('input').nth(3)
+    .fill('123');
+
+  await page.locator('button').nth(0)
+    .click();
+
+  await expect(page).toHaveURL(/dashboard/);
+
+  await page.getByText('Novo Produto', { exact: true })
+    .click();
+
+  await page
+    .getByPlaceholder('Nome do produto')
+    .fill('Teclado Mecânico');
+
+  await page
+    .getByPlaceholder('Quantidade')
+    .fill('10');
+
+  await page
+    .getByText('Salvar', { exact: true })
+    .click();
+
+  await expect(
+    page.getByRole('heading', { name: 'Novo Produto' })
+  ).not.toBeVisible();
+});
